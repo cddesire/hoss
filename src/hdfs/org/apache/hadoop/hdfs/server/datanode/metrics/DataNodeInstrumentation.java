726b7b7753a59d4d780006784e94b84694388bbe
Aspirin Vagrant <cdaspirin@gmail.com>
2017-04-21 09:15:41 +0800

    deleteｕｓｅｌｅｓｓ

--- src/hdfs/org/apache/hadoop/hdfs/server/datanode/metrics/DataNodeInstrumentation.java
+++ src/hdfs/org/apache/hadoop/hdfs/server/datanode/metrics/DataNodeInstrumentation.java
@@ -33,40 +33,44 @@ public class DataNodeInstrumentation implements MetricsSource {
 
   final MetricsRegistry registry = new MetricsRegistry("datanode");
 
-  final MetricMutableCounterLong bytesWritten =
-      registry.newCounter("bytes_written", "", 0L);
-  final MetricMutableCounterLong bytesRead =
-      registry.newCounter("bytes_read", "", 0L);
-  final MetricMutableCounterInt blocksWritten =
-      registry.newCounter("blocks_written", "", 0);
-  final MetricMutableCounterInt blocksRead =
-      registry.newCounter("blocks_read", "", 0);
-  final MetricMutableCounterInt blocksReplicated =
-      registry.newCounter("blocks_replicated", "", 0);
-  final MetricMutableCounterInt blocksRemoved =
-      registry.newCounter("blocks_removed", "", 0);
-  final MetricMutableCounterInt blocksVerified =
-      registry.newCounter("blocks_verified", "", 0);
-  final MetricMutableCounterInt blockVerificationFailures =
-      registry.newCounter("block_verification_failures", "", 0);
-  final MetricMutableCounterInt blocksGetLocalPathInfo = 
-      registry.newCounter("blocks_get_local_pathinfo", "", 0);
+  final MetricMutableCounterLong bytesWritten = registry.newCounter("bytes_written", "", 0L);
 
-  final MetricMutableCounterInt readsFromLocalClient =
-      registry.newCounter("reads_from_local_client", "", 0);
-  final MetricMutableCounterInt readsFromRemoteClient =
-      registry.newCounter("reads_from_remote_client", "", 0);
-  final MetricMutableCounterInt writesFromLocalClient =
-      registry.newCounter("writes_from_local_client", "", 0);
-  final MetricMutableCounterInt writesFromRemoteClient =
-      registry.newCounter("writes_from_remote_client", "", 0);
+  final MetricMutableCounterLong bytesRead = registry.newCounter("bytes_read", "", 0L);
+
+  final MetricMutableCounterInt blocksWritten = registry.newCounter("blocks_written", "", 0);
+
+  final MetricMutableCounterInt blocksRead = registry.newCounter("blocks_read", "", 0);
+
+  final MetricMutableCounterInt blocksReplicated = registry.newCounter("blocks_replicated", "", 0);
+
+  final MetricMutableCounterInt blocksRemoved = registry.newCounter("blocks_removed", "", 0);
+
+  final MetricMutableCounterInt blocksVerified = registry.newCounter("blocks_verified", "", 0);
+
+  final MetricMutableCounterInt blockVerificationFailures = registry.newCounter("block_verification_failures", "", 0);
+
+  final MetricMutableCounterInt blocksGetLocalPathInfo = registry.newCounter("blocks_get_local_pathinfo", "", 0);
+
+  final MetricMutableCounterInt readsFromLocalClient = registry.newCounter("reads_from_local_client", "", 0);
+
+  final MetricMutableCounterInt readsFromRemoteClient = registry.newCounter("reads_from_remote_client", "", 0);
+
+  final MetricMutableCounterInt writesFromLocalClient = registry.newCounter("writes_from_local_client", "", 0);
+
+  final MetricMutableCounterInt writesFromRemoteClient = registry.newCounter("writes_from_remote_client", "", 0);
 
   final MetricMutableStat readBlockOp = registry.newStat("readBlockOp");
+
   final MetricMutableStat writeBlockOp = registry.newStat("writeBlockOp");
+
   final MetricMutableStat blockChecksumOp = registry.newStat("blockChecksumOp");
+
   final MetricMutableStat copyBlockOp = registry.newStat("copyBlockOp");
+
   final MetricMutableStat replaceBlockOp = registry.newStat("replaceBlockOp");
+
   final MetricMutableStat heartbeats = registry.newStat("heartBeats");
+
   final MetricMutableStat blockReports = registry.newStat("blockReports");
 
 
@@ -76,12 +80,10 @@ public class DataNodeInstrumentation implements MetricsSource {
     registry.setContext("dfs").tag("sessionId", "", sessionId);
   }
 
-  //@Override
   public void shutdown() {
     // metrics system shutdown would suffice
   }
 
-  //@Override
   public void resetAllMinMax() {
     readBlockOp.resetMinMax();
     writeBlockOp.resetMinMax();
@@ -92,102 +94,82 @@ public class DataNodeInstrumentation implements MetricsSource {
     blockReports.resetMinMax();
   }
 
-  //@Override
   public void addHeartBeat(long latency) {
     heartbeats.add(latency);
   }
 
-  //@Override
   public void addBlockReport(long latency) {
     blockReports.add(latency);
   }
 
-  //@Override
   public void incrBlocksReplicated(int delta) {
     blocksReplicated.incr(delta);
   }
 
-  //@Override
   public void incrBlocksWritten() {
     blocksWritten.incr();
   }
 
-  //@Override
   public void incrBlocksRemoved(int delta) {
     blocksRemoved.incr(delta);
   }
 
-  //@Override
   public void incrBytesWritten(int delta) {
     bytesWritten.incr(delta);
   }
 
-  //@Override
   public void incrBlockVerificationFailures() {
     blockVerificationFailures.incr();
   }
 
-  //@Override
   public void incrBlocksVerified() {
     blocksVerified.incr();
   }
 
-  //@Override
   public void incrBlocksGetLocalPathInfo() {
     blocksGetLocalPathInfo.incr();
   }
 
-  //@Override
   public void addReadBlockOp(long latency) {
     readBlockOp.add(latency);
   }
 
-  //@Override
   public void incrReadsFromLocalClient() {
     readsFromLocalClient.incr();
   }
 
-  //@Override
   public void incrReadsFromRemoteClient() {
     readsFromRemoteClient.incr();
   }
 
-  //@Override
   public void addWriteBlockOp(long latency) {
     writeBlockOp.add(latency);
   }
 
-  //@Override
   public void incrWritesFromLocalClient() {
     writesFromLocalClient.incr();
   }
 
-  //@Override
   public void incrWritesFromRemoteClient() {
     writesFromRemoteClient.incr();
   }
 
-  //@Override
   public void addReplaceBlockOp(long latency) {
     replaceBlockOp.add(latency);
   }
 
-  //@Override
   public void addCopyBlockOp(long latency) {
     copyBlockOp.add(latency);
   }
 
-  //@Override
   public void addBlockChecksumOp(long latency) {
     blockChecksumOp.add(latency);
   }
 
-  //@Override
   public void incrBytesRead(int delta) {
     bytesRead.incr(delta);
   }
 
-  //@Override
   public void incrBlocksRead() {
     blocksRead.incr();
   }
@@ -197,16 +179,12 @@ public class DataNodeInstrumentation implements MetricsSource {
     registry.snapshot(builder.addRecord(registry.name()), all);
   }
 
-  public static DataNodeInstrumentation create(Configuration conf,
-                                               String storageID) {
+  public static DataNodeInstrumentation create(Configuration conf, String storageID) {
     return create(conf, storageID, DefaultMetricsSystem.INSTANCE);
   }
 
-  public static DataNodeInstrumentation create(Configuration conf,
-                                               String storageID,
-                                               MetricsSystem ms) {
-    return ms.register("DataNode", "DataNode metrics",
-                       new DataNodeInstrumentation(conf, storageID));
+  public static DataNodeInstrumentation create(Configuration conf, String storageID, MetricsSystem ms) {
+    return ms.register("DataNode", "DataNode metrics", new DataNodeInstrumentation(conf, storageID));
   }
 
 }
