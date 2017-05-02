d67c41c0bf31ea6f77a1364f8c6fa74a034ae239
Aspirin Vagrant <cdaspirin@gmail.com>
2017-05-02 08:33:24 +0800

    fix code style

--- src/mapred/org/apache/hadoop/mapreduce/Counters.java
+++ src/mapred/org/apache/hadoop/mapreduce/Counters.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hadoop.mapreduce;
 
 import java.io.DataInput;
@@ -19,18 +37,16 @@ public class Counters implements Writable,Iterable<CounterGroup> {
    */
   private Map<Enum<?>, Counter> cache = new IdentityHashMap<Enum<?>, Counter>();
 
-  private TreeMap<String, CounterGroup> groups = 
-      new TreeMap<String, CounterGroup>();
+  private TreeMap<String, CounterGroup> groups = new TreeMap<String, CounterGroup>();
   
-  public Counters() {
-  }
+  public Counters() {}
   
   public Counters(org.apache.hadoop.mapred.Counters counters) {
     for(org.apache.hadoop.mapred.Counters.Group group: counters) {
       String name = group.getName();
       CounterGroup newGroup = new CounterGroup(name, group.getDisplayName());
       groups.put(name, newGroup);
-      for(Counter counter: group) {
+      for (Counter counter: group) {
         newGroup.addCounter(counter);
       }
     }
