8a5282fbdcbb897a640baa39ee4f94a00c51760b
Aspirin Vagrant <cdaspirin@gmail.com>
2017-05-03 08:26:15 +0800

    fix code style

--- src/mapred/org/apache/hadoop/mapreduce/security/token/delegation/DelegationTokenIdentifier.java
+++ src/mapred/org/apache/hadoop/mapreduce/security/token/delegation/DelegationTokenIdentifier.java
@@ -29,29 +29,28 @@ import org.apache.hadoop.security.token.delegation.AbstractDelegationTokenIdenti
 public class DelegationTokenIdentifier 
   extends AbstractDelegationTokenIdentifier {
   
-  public static final Text MAPREDUCE_DELEGATION_KIND = 
-    new Text("MAPREDUCE_DELEGATION_TOKEN");
+  public static final Text MAPREDUCE_DELEGATION_KIND = new Text("MAPREDUCE_DELEGATION_TOKEN");
 
-/**
- * Create an empty delegation token identifier for reading into.
- */
-public DelegationTokenIdentifier() {
-}
+  /**
+   * Create an empty delegation token identifier for reading into.
+   */
+  public DelegationTokenIdentifier() {
+  }
 
-/**
- * Create a new delegation token identifier
- * @param owner the effective username of the token owner
- * @param renewer the username of the renewer
- * @param realUser the real username of the token owner
- */
-public DelegationTokenIdentifier(Text owner, Text renewer, Text realUser) {
-  super(owner, renewer, realUser);
-}
+  /**
+   * Create a new delegation token identifier
+   * @param owner the effective username of the token owner
+   * @param renewer the username of the renewer
+   * @param realUser the real username of the token owner
+   */
+  public DelegationTokenIdentifier(Text owner, Text renewer, Text realUser) {
+    super(owner, renewer, realUser);
+  }
 
-@Override
-public Text getKind() {
-  return MAPREDUCE_DELEGATION_KIND;
-}
+  @Override
+  public Text getKind() {
+    return MAPREDUCE_DELEGATION_KIND;
+  }
 
 }
 
