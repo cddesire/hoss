132b137a7d0cada88b6ed230f77a1f9570b9dd7c
changdong.cd <changdong.cd@alibaba-inc.com>
2017-05-07 15:24:21 +0800

    fix code style

--- src/mapred/org/apache/hadoop/mapred/MapTaskRunner.java
+++ src/mapred/org/apache/hadoop/mapred/MapTaskRunner.java
@@ -23,45 +23,40 @@ import org.apache.hadoop.mapred.TaskTracker.TaskInProgress;
 
 /** Runs a map task. */
 class MapTaskRunner extends TaskRunner {
-  
+
   public MapTaskRunner(TaskInProgress task, TaskTracker tracker, JobConf conf,
-                       TaskTracker.RunningJob rjob) 
-  throws IOException {
+                       TaskTracker.RunningJob rjob) throws IOException {
     super(task, tracker, conf, rjob);
   }
-  
+
   /** Delete any temporary files from previous failed attempts. */
   public boolean prepare() throws IOException {
     if (!super.prepare()) {
       return false;
     }
-    
+
     mapOutputFile.removeAll();
     return true;
   }
 
   /** Delete all of the temporary map output files. */
   public void close() throws IOException {
-    LOG.info(getTask()+" done; removing files.");
+    LOG.info(getTask() + " done; removing files.");
     mapOutputFile.removeAll();
   }
 
   @Override
   public String getChildJavaOpts(JobConf jobConf, String defaultValue) {
-    String user = 
-      jobConf.get(JobConf.MAPRED_MAP_TASK_JAVA_OPTS, 
-        super.getChildJavaOpts(jobConf, 
-            JobConf.DEFAULT_MAPRED_TASK_JAVA_OPTS));
-    String admin = 
-      jobConf.get(TaskRunner.MAPRED_MAP_ADMIN_JAVA_OPTS,
-        TaskRunner.DEFAULT_MAPRED_ADMIN_JAVA_OPTS);
+    String user = jobConf.get(JobConf.MAPRED_MAP_TASK_JAVA_OPTS,
+                  super.getChildJavaOpts(jobConf, JobConf.DEFAULT_MAPRED_TASK_JAVA_OPTS));
+    String admin = jobConf.get(TaskRunner.MAPRED_MAP_ADMIN_JAVA_OPTS,
+                  TaskRunner.DEFAULT_MAPRED_ADMIN_JAVA_OPTS);
     return user + " " + admin;
   }
 
   @Override
   public int getChildUlimit(JobConf jobConf) {
-    return jobConf.getInt(JobConf.MAPRED_MAP_TASK_ULIMIT, 
-                          super.getChildUlimit(jobConf));
+    return jobConf.getInt(JobConf.MAPRED_MAP_TASK_ULIMIT, super.getChildUlimit(jobConf));
   }
 
   @Override
