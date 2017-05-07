ca080f0783ba667a5d80926d785eebebc3b28473
changdong.cd <changdong.cd@alibaba-inc.com>
2017-05-07 15:32:03 +0800

    fix code style

--- src/mapred/org/apache/hadoop/mapred/MapRunner.java
+++ src/mapred/org/apache/hadoop/mapred/MapRunner.java
@@ -23,9 +23,8 @@ import java.io.IOException;
 import org.apache.hadoop.util.ReflectionUtils;
 
 /** Default {@link MapRunnable} implementation.*/
-public class MapRunner<K1, V1, K2, V2>
-    implements MapRunnable<K1, V1, K2, V2> {
-  
+public class MapRunner<K1, V1, K2, V2> implements MapRunnable<K1, V1, K2, V2> {
+
   private Mapper<K1, V1, K2, V2> mapper;
   private boolean incrProcCount;
 
@@ -33,24 +32,23 @@ public class MapRunner<K1, V1, K2, V2>
   public void configure(JobConf job) {
     this.mapper = ReflectionUtils.newInstance(job.getMapperClass(), job);
     //increment processed counter only if skipping feature is enabled
-    this.incrProcCount = SkipBadRecords.getMapperMaxSkipRecords(job)>0 && 
-      SkipBadRecords.getAutoIncrMapperProcCount(job);
+    this.incrProcCount = SkipBadRecords.getMapperMaxSkipRecords(job) > 0 &&
+                         SkipBadRecords.getAutoIncrMapperProcCount(job);
   }
 
   public void run(RecordReader<K1, V1> input, OutputCollector<K2, V2> output,
-                  Reporter reporter)
-    throws IOException {
+                  Reporter reporter) throws IOException {
     try {
       // allocate key & value instances that are re-used for all entries
       K1 key = input.createKey();
       V1 value = input.createValue();
-      
+
       while (input.next(key, value)) {
         // map pair to output
         mapper.map(key, value, output, reporter);
-        if(incrProcCount) {
-          reporter.incrCounter(SkipBadRecords.COUNTER_GROUP, 
-              SkipBadRecords.COUNTER_MAP_PROCESSED_RECORDS, 1);
+        if (incrProcCount) {
+          reporter.incrCounter(SkipBadRecords.COUNTER_GROUP,
+                               SkipBadRecords.COUNTER_MAP_PROCESSED_RECORDS, 1);
         }
       }
     } finally {
