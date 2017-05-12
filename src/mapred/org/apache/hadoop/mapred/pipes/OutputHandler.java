386a5bf8dbc05f6eba79fa9655e161feb0c7c662
Aspirin Vagrant <cdaspirin@gmail.com>
2017-05-12 09:37:29 +0800

    fix code style

--- src/mapred/org/apache/hadoop/mapred/pipes/OutputHandler.java
+++ src/mapred/org/apache/hadoop/mapred/pipes/OutputHandler.java
@@ -36,19 +36,16 @@ import org.apache.hadoop.mapred.Reporter;
 /**
  * Handles the upward (C++ to Java) messages from the application.
  */
-class OutputHandler<K extends WritableComparable,
-                    V extends Writable>
-  implements UpwardProtocol<K, V> {
-  
+class OutputHandler<K extends WritableComparable, V extends Writable> implements UpwardProtocol<K, V> {
+
   private Reporter reporter;
   private OutputCollector<K, V> collector;
   private float progressValue = 0.0f;
   private boolean done = false;
-  
+
   private Throwable exception = null;
-  RecordReader<FloatWritable,NullWritable> recordReader = null;
-  private Map<Integer, Counters.Counter> registeredCounters = 
-    new HashMap<Integer, Counters.Counter>();
+  RecordReader<FloatWritable, NullWritable> recordReader = null;
+  private Map<Integer, Counters.Counter> registeredCounters = new HashMap<Integer, Counters.Counter>();
 
   private String expectedDigest = null;
   private boolean digestReceived = false;
@@ -57,8 +54,8 @@ class OutputHandler<K extends WritableComparable,
    * @param collector the "real" collector that takes the output
    * @param reporter the reporter for reporting progress
    */
-  public OutputHandler(OutputCollector<K, V> collector, Reporter reporter, 
-                       RecordReader<FloatWritable,NullWritable> recordReader,
+  public OutputHandler(OutputCollector<K, V> collector, Reporter reporter,
+                       RecordReader<FloatWritable, NullWritable> recordReader,
                        String expectedDigest) {
     this.reporter = reporter;
     this.collector = collector;
@@ -76,8 +73,7 @@ class OutputHandler<K extends WritableComparable,
   /**
    * The task output a record with a partition number attached.
    */
-  public void partitionedOutput(int reduce, K key, 
-                                V value) throws IOException {
+  public void partitionedOutput(int reduce, K key, V value) throws IOException {
     PipesPartitioner.setNextPartition(reduce);
     collector.collect(key, value);
   }
@@ -97,7 +93,7 @@ class OutputHandler<K extends WritableComparable,
   public void progress(float progress) throws IOException {
     progressValue = progress;
     reporter.progress();
-    
+
     if (recordReader != null) {
       progressKey.set(progress);
       recordReader.next(progressKey, nullValue);
@@ -160,12 +156,12 @@ class OutputHandler<K extends WritableComparable,
       throw new IOException("Invalid counter with id: " + id);
     }
   }
-  
+
   public synchronized boolean authenticate(String digest) throws IOException {
     boolean success = true;
     if (!expectedDigest.equals(digest)) {
       exception = new IOException("Authentication Failed: Expected digest="
-          + expectedDigest + ", received=" + digestReceived);
+                                  + expectedDigest + ", received=" + digestReceived);
       success = false;
     }
     digestReceived = true;
@@ -180,7 +176,7 @@ class OutputHandler<K extends WritableComparable,
    * @throws InterruptedException
    */
   synchronized void waitForAuthentication()
-      throws IOException, InterruptedException {
+  throws IOException, InterruptedException {
     while (digestReceived == false && exception == null) {
       wait();
     }
